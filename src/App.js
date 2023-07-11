d8c93d1802ff67b57f551ee46f26a4cb23a02ae4
kimdev <kevohkyme35@gmail.com>
2023-07-11 12:51:46 +0300

    to-do-list

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import TodoList from './components/TodoList';
+import './App.css';
+
+function App() {
+  return (
+    <div className="to-do-app">
+      
+      <TodoList/>
+    </div>
+  );
+}
+
+export default App;
